37e3086b191229d74f9124b00813b19eb142df07
LukasSkaane <270139160@yoobeestudent.ac.nz>
2021-11-09 01:04:28 +0100

    Added declarations in StorageHandling.h

---
+++ Group assignment NZ School System/StorageHandling.h
@@ -0,0 +1,14 @@
+#ifndef STORAGEHANDLING_H
+#define STORAGEHANDLING_H
+
+#include "Structures.h"
+
+using namespace std;
+
+void saveStudentToFile(Student&);
+void saveParentToFile(Parent&);
+void saveTeacherToFile(Teacher&);
+
+
+
+#endif // !STORAGEHANDLING_H
